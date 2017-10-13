0721d9a3e34dadab9effad3f0e5d5be514d21e99
Xiang 'Anthony' Chen <xiangchen@cmu.edu>
2017-10-13 19:13:15 -0400

    minor update

--- xray.js
+++ xray.js
@@ -1,26 +1,12 @@
+////////////////////////////////////////////////////////////////////////////////////
+//
+//	incomplete library of javascript array extension
+//
+//	by xiangchen@acm.org, v0.0, 10/2017
+//
+////////////////////////////////////////////////////////////////////////////////////
+
 'use strict';
-
-// module.exports = {
-// 	remove: function(elm, compFunc) {
-// 		var toRemove = [];
-// 		for (var i = this.length - 1; i >= 0; i--) {
-// 			var equal = undefined;
-// 			if (compFunc != undefined) {
-// 				equal = compFunc(elm, this[i]);
-// 			} else {
-// 				equal = elm == this[i];
-// 			}
-
-// 			if (equal) {
-// 				toRemove.push(i);
-// 			}
-// 		}
-
-// 		for (var i = toRemove.length - 1; i >= 0; i--) {
-// 			this.splice(toRemove[i], 1);
-// 		}
-// 	}
-// }
 
 Array.prototype.remove = function (elm, compFunc) {
 	var toRemove = [];
